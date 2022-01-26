e1bc298509cd1814a23f5e151647acbd5297b151
Bazgha19 <sbs019190@gmail.com>
2022-01-26 19:20:34 +0530

    Even Odd Program

--- Basic Program/EvenOdd.java
+++ Basic Program/EvenOdd.java
@@ -1,9 +1,14 @@
 import java.util.Scanner;
-
 public class EvenOdd{
     public static void main(String[] args) {
         try (Scanner s = new Scanner(System.in)) {
-            System.out.println("Number is "+ s.nextInt());
+            int number = s.nextInt();
+            if (number%2==0){
+                System.out.println(number + " is even number");
+            }
+            else {
+                System.out.println(number + " is odd number");
+            }
         }
     }
 }
