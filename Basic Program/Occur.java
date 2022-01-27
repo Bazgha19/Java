15261970ca6a4806a6f1b7ce95d6a92ef666653f
Bazgha19 <sbs019190@gmail.com>
2022-01-27 13:22:37 +0530

    Check occurrence of number

--- Basic Program/Occur.java
+++ Basic Program/Occur.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Occur {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.print("Enter number: ");
         int n = in.nextInt();
+        System.out.print("Enter the number to check the occurrence: ");
         int number = in.nextInt();
         int count = 0;
         while(n>0){
@@ -13,6 +15,6 @@ public class Occur {
             }
             n = n/10;
         }
-        System.out.println(count);
+        System.out.println("Number "+number+" occurred "+count+ " times.");
     }
 }
