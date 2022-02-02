59818cdf0a048201214dc157ff8f96c950fc7a24
Bazgha19 <sbs019190@gmail.com>
2022-02-02 13:01:22 +0530

    square root in java

---
+++ Basic Program/Sqrt.java
@@ -0,0 +1,7 @@
+public class Sqrt {
+    public static void main(String[] args) {
+        int no = 4;
+        double sqrt = Math.pow(no,0.5);
+        System.out.println(Math.round(sqrt));
+    }
+}
