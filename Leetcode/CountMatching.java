55e063dea8198d278949be6f791b393a09342365
Bazgha19 <sbs019190@gmail.com>
2022-02-09 20:03:04 +0530

    Count matching leetcode problem.

--- Leetcode/CountMatching.java
+++ Leetcode/CountMatching.java
@@ -9,17 +9,17 @@ public class CountMatching {
         String ruleKey = "color";
         String ruleValue = "silver";
 
-        countMatches(arr, ruleKey, ruleValue);
+//        countMatches(arr, ruleKey, ruleValue);
     }
-    static void countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int res = 0;
-
-        for(int i = 0 ;i<items.size();i++){
-            if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)) res++;
-            if(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)) res++;
-            if(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)) res++;
-        }
-
-        System.out.println(res);
-    }
+//    static void countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+//        int res = 0;
+//
+//        for(int i = 0 ;i<items.size();i++){
+//            if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)) res++;
+//            if(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)) res++;
+//            if(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)) res++;
+//        }
+//
+//        System.out.println(res);
+//    }
 }
