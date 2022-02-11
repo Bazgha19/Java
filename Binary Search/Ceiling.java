3da93f06ccba58b379f295b351c1c456fbbcf4c9
Bazgha19 <sbs019190@gmail.com>
2022-02-11 12:09:16 +0530

    Ceil value of a number.

---
+++ Binary Search/Ceiling.java
@@ -0,0 +1,27 @@
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] arr = {-2,-1,0,1,2,3,4,5,7,8,9,10};
+        int target =6;
+        System.out.println(ceil(arr,target));
+    }
+
+    //Ceil Value: Greater than or equal to target element
+    static int ceil(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start<=end){
+            int mid = start + (end-start)/2;
+            if (target<arr[mid]){
+                end = mid-1;
+            }
+            else if(target>arr[mid]) {
+                start = mid+1;
+            }
+            else{
+                return arr[mid];
+            }
+        }
+        return arr[start];
+    }
+}
