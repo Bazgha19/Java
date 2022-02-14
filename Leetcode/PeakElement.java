ba1871532141a7062dda22d03ddbc123bb025e9d
Bazgha19 <sbs019190@gmail.com>
2022-02-14 12:42:17 +0530

    Index of peak element leetcode problem.

---
+++ Leetcode/PeakElement.java
@@ -0,0 +1,21 @@
+//Problem Statement: https://leetcode.com/problems/find-peak-element
+public class PeakElement {
+    public static void main(String[] args) {
+        int[] arr = {1,2,1,3,5,6,4};
+        System.out.println(findPeakElement(arr));
+
+    }
+    static int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        while (start<end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]>nums[mid+1]){
+                end = mid;
+            } else{
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}
