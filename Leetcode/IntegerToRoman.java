fee8276fbeb9abdd298a25a5b6a80db8a125c6e6
Bazgha19 <sbs019190@gmail.com>
2022-04-04 06:53:45 +0530

    Integer to roman leetcode problem.

---
+++ Leetcode/IntegerToRoman.java
@@ -0,0 +1,14 @@
+//Problem Statement: https://leetcode.com/problems/integer-to-roman/
+public class IntegerToRoman {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(intToRoman(n));
+    }
+    static String intToRoman(int num) {
+        String M[] = {"", "M", "MM", "MMM"};
+        String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+    }
+}
