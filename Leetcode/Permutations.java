de7c4e311c1e0b7bb6a10ae6c79f21528aee5ba0
Bazgha19 <sbs019190@gmail.com>
2022-04-06 23:34:40 +0530

    Permutations leetcode problem.

---
+++ Leetcode/Permutations.java
@@ -0,0 +1,35 @@
+//Problem Statement: https://leetcode.com/problems/permutations/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        System.out.println(permute(nums));
+    }
+    static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> permutations = new ArrayList<>();
+        if (nums.length == 0) {
+            return permutations;
+        }
+
+        collectPermutations(nums, 0, new ArrayList<>(), permutations);
+        return permutations;
+    }
+
+    static void collectPermutations(int[] nums, int start, List<Integer> permutation,
+                                     List<List<Integer>>  permutations) {
+
+        if (permutation.size() == nums.length) {
+            permutations.add(permutation);
+            return;
+        }
+
+        for (int i = 0; i <= permutation.size(); i++) {
+            List<Integer> newPermutation = new ArrayList<>(permutation);
+            newPermutation.add(i, nums[start]);
+            collectPermutations(nums, start + 1, newPermutation, permutations);
+        }
+    }
+}
